0de82c0af44f167acdd0e0e550eada29d3bfae09
Isha Kashif <ishak@r0login1.localdomain.hpc.udel.edu>
2026-04-10 12:21:00 -0400

    Adding pthreads and openmp versions with script output (to review)

---
+++ image_pthreads.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <string.h>
+#include <pthread.h>
+#include "image.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+Matrix algorithms[] = {
+    {{0,-1,0},{-1,4,-1},{0,-1,0}},
+    {{0,-1,0},{-1,5,-1},{0,-1,0}},
+    {{1/9.0,1/9.0,1/9.0},{1/9.0,1/9.0,1/9.0},{1/9.0,1/9.0,1/9.0}},
+    {{1.0/16,1.0/8,1.0/16},{1.0/8,1.0/4,1.0/8},{1.0/16,1.0/8,1.0/16}},
+    {{-2,-1,0},{-1,1,1},{0,1,2}},
+    {{0,0,0},{0,1,0},{0,0,0}}
+};
+
+#define NUM_THREADS 4
+
+typedef struct {
+    Image *srcImage;
+    Image *destImage;
+    Matrix algorithm;
+    int start_row;
+    int end_row;
+} ThreadData;
+
+uint8_t getPixelValue(Image* srcImage, int x, int y, int bit, Matrix algorithm) {
+    int px, mx, py, my, span;
+    span = srcImage->width * srcImage->bpp;
+    px = x + 1; py = y + 1; mx = x - 1; my = y - 1;
+    if (mx < 0) mx = 0;
+    if (my < 0) my = 0;
+    if (px >= srcImage->width)  px = srcImage->width - 1;
+    if (py >= srcImage->height) py = srcImage->height - 1;
+    uint8_t result =
+        algorithm[0][0] * srcImage->data[Index(mx, my, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[0][1] * srcImage->data[Index(x,  my, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[0][2] * srcImage->data[Index(px, my, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[1][0] * srcImage->data[Index(mx, y,  srcImage->width, bit, srcImage->bpp)] +
+        algorithm[1][1] * srcImage->data[Index(x,  y,  srcImage->width, bit, srcImage->bpp)] +
+        algorithm[1][2] * srcImage->data[Index(px, y,  srcImage->width, bit, srcImage->bpp)] +
+        algorithm[2][0] * srcImage->data[Index(mx, py, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[2][1] * srcImage->data[Index(x,  py, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[2][2] * srcImage->data[Index(px, py, srcImage->width, bit, srcImage->bpp)];
+    return result;
+}
+
+void* thread_convolute(void* arg) {
+    ThreadData* td = (ThreadData*) arg;
+    int row, pix, bit;
+    for (row = td->start_row; row < td->end_row; row++) {
+        for (pix = 0; pix < td->srcImage->width; pix++) {
+            for (bit = 0; bit < td->srcImage->bpp; bit++) {
+                td->destImage->data[Index(pix, row, td->srcImage->width, bit, td->srcImage->bpp)] =
+                    getPixelValue(td->srcImage, pix, row, bit, td->algorithm);
+            }
+        }
+    }
+    return NULL;
+}
+
+void convolute(Image* srcImage, Image* destImage, Matrix algorithm) {
+    int i;
+    pthread_t threads[NUM_THREADS];
+    ThreadData threadData[NUM_THREADS];
+    int rows_per_thread = srcImage->height / NUM_THREADS;
+    for (i = 0; i < NUM_THREADS; i++) {
+        threadData[i].srcImage  = srcImage;
+        threadData[i].destImage = destImage;
+        memcpy(threadData[i].algorithm, algorithm, sizeof(Matrix));
+        threadData[i].start_row = i * rows_per_thread;
+        threadData[i].end_row = (i == NUM_THREADS - 1) ? srcImage->height : (i + 1) * rows_per_thread;
+        pthread_create(&threads[i], NULL, thread_convolute, &threadData[i]);
+    }
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+int Usage() {
+    printf("Usage: image <filename> <type>\n\twhere type is one of (edge, sharpen, blur, gauss, emboss, identity)\n");
+    return -1;
+}
+
+enum KernelTypes GetKernelType(char* type) {
+    if (!strcmp(type, "edge"))         return EDGE;
+    else if (!strcmp(type, "sharpen")) return SHARPEN;
+    else if (!strcmp(type, "blur"))    return BLUR;
+    else if (!strcmp(type, "gauss"))   return GAUSE_BLUR;
+    else if (!strcmp(type, "emboss"))  return EMBOSS;
+    else return IDENTITY;
+}
+
+int main(int argc, char** argv) {
+    long t1, t2;
+    t1 = time(NULL);
+    stbi_set_flip_vertically_on_load(0);
+    if (argc != 3) return Usage();
+    char* fileName = argv[1];
+    if (!strcmp(argv[1], "pic4.jpg") && !strcmp(argv[2], "gauss")) {
+        printf("You have applied a gaussian filter to Gauss which has caused a singularity.\n");
+    }
+    enum KernelTypes type = GetKernelType(argv[2]);
+    Image srcImage, destImage;
+    srcImage.data = stbi_load(fileName, &srcImage.width, &srcImage.height, &srcImage.bpp, 0);
+    if (!srcImage.data) {
+        printf("Error loading file %s.\n", fileName);
+        return -1;
+    }
+    destImage.bpp    = srcImage.bpp;
+    destImage.height = srcImage.height;
+    destImage.width  = srcImage.width;
+    destImage.data   = malloc(sizeof(uint8_t) * destImage.width * destImage.bpp * destImage.height);
+    convolute(&srcImage, &destImage, algorithms[type]);
+    stbi_write_png("output.png", destImage.width, destImage.height, destImage.bpp, destImage.data, destImage.width * destImage.bpp);
+    stbi_image_free(srcImage.data);
+    free(destImage.data);
+    t2 = time(NULL);
+    printf("Took %ld seconds\n", t2 - t1);
+    return 0;
+}
